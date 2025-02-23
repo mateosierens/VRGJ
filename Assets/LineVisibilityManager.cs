f995a20acd40e6a94285d8ddf48dc9f83a685ccc
trinm <tim.sanders@student.uantwerpen.be>
2025-02-23 10:41:59 +0100

    line visibility

---
+++ Assets/LineVisibilityManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class LineVisibilityManager : MonoBehaviour
+{
+    public XRInteractorLineVisual _lineRenderer;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _lineRenderer.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void OnUiEntered()
+    {
+        _lineRenderer.enabled = true;
+    }
+    
+    public void OnUiExited()
+    {
+        _lineRenderer.enabled = false;
+    }
+}
